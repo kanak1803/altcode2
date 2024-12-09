d76a39eeee333e79efdabe252f8aa9cde0f201cc
kanak1803 <kanak1803@gmail.com>
2024-12-09 22:16:35 +0530

    added AllCategoryMenu page

---
+++ src/app/category/page.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function AllCategoryMenu() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-teal-700 text-white">
+  <div className="text-center p-6 bg-teal-700 text-white rounded-lg">
+    <h1 className="text-3xl font-bold mb-4">Coming Soon!</h1>
+    <p>We are working hard to bring this category to you. Stay tuned!</p>
+  </div>
+</div>
+  );
+}
+
+export default AllCategoryMenu;
